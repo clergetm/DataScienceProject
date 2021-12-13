9f7d9e037a8c989b8757b57edbf6a3858c022c3f
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2021-12-13 12:15:57 -0500

    Rename and correction

--- Train/main.py
+++ Train/main.py
@@ -1,15 +1,15 @@
 import time
 import glob
-from math import ceil, floor
 
 import numpy as np
 import pandas as pd
-from sklearn import preprocessing
+from scipy.stats import zscore  # Normalize DataFrame
 
 from Utils.Get import get_data, get_duplicate, get_nan, get_specs
+from Utils.Protocol import protocol
 
 ########################################################################################################################
-#                                                    USER PARAMETERS                                                   #
+#                                                  USER PARAMETERS                                                     #
 ########################################################################################################################
 
 # Path to the training dataset from the Train/main.py file
@@ -17,7 +17,6 @@ path = "../Files/Docs/MHEALTHDATASET/TrainSet/"
 
 # List of files used as training set
 files = [f"mHealth_subject{str(i)}.txt" for i in range(1, 2)]
-
 
 if __name__ == '__main__':
 	start_time = time.time()
@@ -40,39 +39,44 @@ if __name__ == '__main__':
 		# The Label '0' need to be deleted : it corresponds to no class
 		df.drop(index=df[df['Label'] == 0].index, axis='index', inplace=True)
 		
-		classes = []
-		# Get each possible class
-		class_id = np.unique(df['Label'])
+		activities = []
+		# Get each possible activity
+		activities_id = np.unique(df['Label'])
 		
+		# activity_min will be useful for flattening all the activity at the same amount of instances
+		activity_min = df.shape[0]  # shape[0] to initialize at a high value
 		
-		# class_min will be useful for flattening all the class at the same amount of instances
-		class_min = df.shape[0]
-		
-		for id_value in class_id:
-			_class = df[df['Label'] == id_value]
-			instances = _class.shape[0]  # Get the number of instances
+		for id_value in activities_id:
+			activity = df[df['Label'] == id_value]
+			instances = activity.shape[0]  # Get the number of instances
 			
 			# Change the min value
-			class_min = instances if instances < class_min else class_min
-			# print(f"{id_value}: {instances}")
-			classes.append(_class)
-		
-		# Now that we have the classes and the minimum : we need to flatten the classes
-		flatten_classes = []
-		for _class in classes:
-			if _class.shape[0] > class_min:
-				#  Get the minimum number of instances chosen at random
-				_class = _class.sample(class_min)
+			activity_min = instances if instances < activity_min else activity_min
 			
-			_class.reset_index(inplace=True)
-			flatten_classes.append(_class)
+			# print(f"{id_value}: {instances}")
+			activities.append(activity)
 		
-		print(flatten_classes)
+		# Now that we have the activities and a minimum : we need to flatten them
+		flatten_activities = []
+
+		for activity in activities:
+			if activity.shape[0] > activity_min:
+				
+				#  Get the minimum number of instances chosen at random
+				activity = activity.sample(activity_min)
+				
+				# Normalizes DataFrame using the z score method
+				activity.apply(zscore)
+				
+			activity.reset_index(inplace=True, drop=True)  # Reset the index and not keep the previous one
+			flatten_activities.append(activity)
+		
 		print("X" * 100)
-		
-		# After the cleaning and the classes' creation, we can launch the protocol
-		protocol(flatten_classes, time_window_length=0, non_overlapping_length=0,
+	
+		# After the cleaning and the activities' creation, we can launch the protocol
+		protocol(flatten_activities, time_window_length=0, non_overlapping_length=0,
 		         # split to remove the extension
-		         pickle_file=file.split(sep='.')[0], time_slider_folder=file.split(sep='.')[0])
+		         pickle_file=file.split(sep='.')[0], folder=file.split(sep='.')[0])
+	
 	end_time = time.time()
 	print(f"total time = {end_time - start_time}")
