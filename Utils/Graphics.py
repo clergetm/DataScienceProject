0270189eac598a8e0109f1caa9028205d3c09612
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2021-12-17 22:02:59 -0500

    Update Utils
    Add Graphics.plot_stats
    Utils.normalize et Utils.to_flatten_df

--- Utils/Graphics.py
+++ Utils/Graphics.py
@@ -1,30 +1,50 @@
-import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-from Utils.ExtractionUtils.Extraction import get_specs_mean, get_specs_variance, get_specs_std
+from Utils.Utils import delete_files
 
 
-def plot_stats(subjects, save_path):
+def plot_stats(subjects, features, save_path):
 	"""
 	Plot the stats of the variables
-	:param pd.DataFrame df: The values of the Variable
-	:param int label: the number (+1) corresponding to an activity
-	:param str column_name: the name of the column used in df
-	:param str save_path: the folder path where to save the plot
+	:param list subjects: list of all subject dataset
+	:param list features: list of all features to plot
+	:param str save_path: the path of the folder where the plot are saved
 	"""
-	#  Get the corresponding activity
-	activity = ["Standing still (1 min) ",
-	            "Sitting and relaxing (1 min) ",
-	            "Lying down (1 min) ",
-	            "Walking (1 min)", "Climbing stairs (1 min) ",
-	            "Waist bends forward (20x) ",
-	            "Frontal elevation of arms (20x)",
-	            "Knees bending (crouching) (20x)",
-	            "Cycling (1 min)",
-	            "Jogging (1 min)",
-	            "Running (1 min)",
-	            "Jump front & back (20x)"][
-		label - 1]  # Minus one because the original list is composed with the first element as an empty element
+	# Deleting precedent plot
+	delete_files(save_path)
+	# List of all possible activity
+	activities_name = ["Standing still (1 min) ",
+	                   "Sitting and relaxing (1 min) ",
+	                   "Lying down (1 min) ",
+	                   "Walking (1 min)", "Climbing stairs (1 min) ",
+	                   "Waist bends forward (20x) ",
+	                   "Frontal elevation of arms (20x)",
+	                   "Knees bending (crouching) (20x)",
+	                   "Cycling (1 min)",
+	                   "Jogging (1 min)",
+	                   "Running (1 min)",
+	                   "Jump front & back (20x)"]
 	
-	activities_id = np.unique(df['Label'])
+	# Go through each activity
+	for i in range(len(activities_name)):
+		label = i + 1  # +1 because the 'Label' 0 corresponds to null and was deleted before
+		# Then go through each feature in each subject
+		tmp_subjects_sample_activity = {}
+		
+		for feature in features:
+			# Get the data of all subject for the current activity
+			for subject in subjects:
+				# Get the data of this subject for this feature
+				tmp_subjects_sample_activity[subject['ID'].loc[0]] = subject[subject['Label'] == label][feature]
+			
+			# Plotting
+			to_plot = pd.DataFrame(tmp_subjects_sample_activity)
+			plt.figure(figsize=(10, 100))
+			to_plot.plot()
+			plt.title(label=f"Compare subjects on {activities_name[i]} / {feature}")
+			plt.savefig(fname=save_path + f"{label}_{activities_name[i]}_{feature}.png", dpi=100, format='png')
+			plt.clf()
+			plt.cla()
+			plt.close(plt.figure())
+			
--- Utils/Utils.py
+++ Utils/Utils.py
@@ -1,6 +1,9 @@
 import glob
 import os
 
+import numpy as np
+import pandas as pd
+
 
 def delete_files(path):
 	"""
@@ -11,3 +14,55 @@ def delete_files(path):
 		for f in glob.glob(path + "/*"):
 			os.remove(f)
 			
+
+def to_flatten_df(to_flat, ceil) -> pd.DataFrame:
+	"""
+	To Flatten the DataFrame at the same ceil for each activity
+	:param pd.DataFrame to_flat: the DataFrame to flatten
+	:param ceil: the ceil for each activity
+	:return: The flattened DataFrame
+	:rtype: pd.DataFrame
+	"""
+	# List that will contain flattened activities
+	flattened_activities = []
+	
+	# Loop for each activity
+	for id_activity in np.unique(to_flat['Label']):
+		tmp_activity = to_flat[to_flat['Label'] == id_activity]
+		
+		# Compare the length of the activity to the ceil value
+		if tmp_activity.shape[0] > ceil:
+
+			#  Get the minimum number of instances chosen at random
+			tmp_activity = tmp_activity.sample(ceil)
+				
+		flattened_activities.append(tmp_activity)
+	# return a Flattened DataFrame
+	return pd.concat(flattened_activities, ignore_index=True)
+
+
+def normalize(df, minus_columns=-100) -> pd.DataFrame:
+	"""
+	Normalize the given DataFrame
+	:param pd.DataFrame df: The DataFrame to Normalize
+	:param int minus_columns: The number of column to NOT Normalize starting from the rightmost column
+		The default value of -100 correspond to removing 0 column
+	:return result: the normalized DataFrame
+	:rtype result: pd.DataFrame
+	
+	see also: From https://stackoverflow.com/questions/26414913/normalize-columns-of-pandas-data-frame
+	"""
+	
+	# Create a copy of the DataFrame to work with
+	result = df.copy()
+	
+	for feature_name in df.columns[:-minus_columns]:
+		# Get minimum and maximum values of the feature
+		min_value = df[feature_name].min()
+		max_value = df[feature_name].max()
+		
+		# Normalize the feature
+		result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+		
+	# Return the normalized DataFrame
+	return result
